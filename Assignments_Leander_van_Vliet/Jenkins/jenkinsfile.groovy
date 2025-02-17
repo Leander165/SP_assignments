c1f81de9b0760e8531c9aaeb97dcc9a1f5f574fe
Leander165 <91758895+Leander165@users.noreply.github.com>
2025-02-17 22:03:55 +0100

    Add files via upload

--- Assignments_Leander_van_Vliet/Jenkins/jenkinsfile.groovy
+++ Assignments_Leander_van_Vliet/Jenkins/jenkinsfile.groovy
@@ -12,5 +12,25 @@ pipeline {
                 git branch: 'main', url: 'https://github.com/Leander165/SP_assignments.git'
             }
         }
-    }
-}
+        stage('Install Dependencies') {
+            steps {
+                script {
+                    sh '''
+                    python --version
+                    pip --version
+                    pip install --upgrade pip
+                    pip install --upgrade robotframework robotframework-seleniumlibrary
+                    robot --version
+                    '''
+                }
+            }
+        }
+        stage('Run UI Tests') {
+            steps {
+                script {
+                    sh '''
+                    python -m robot -d results $(pwd)/Assignments_Leander_van_Vliet/UITests/BuyAShirtEndToEndTest/BuyAShirtEndToEndTest.robot || echo "⚠️ Robot Framework test failed with exit code $?"
+                    '''
+                }
+            }
+        }
