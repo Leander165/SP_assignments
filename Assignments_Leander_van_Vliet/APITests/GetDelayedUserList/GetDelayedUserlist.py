a0312f0cacfca17cbe4020d3a1ea4e5f44724705
Leander165 <91758895+Leander165@users.noreply.github.com>
2025-02-18 15:32:32 +0100

    Add files via upload

--- Assignments_Leander_van_Vliet/APITests/GetDelayedUserList/GetDelayedUserlist.py
+++ Assignments_Leander_van_Vliet/APITests/GetDelayedUserList/GetDelayedUserlist.py
@@ -1,8 +1,29 @@
 # With some delay (like 3 seconds), you will request a list with personal information from a database. The response of this request is the list with all the information.
+# A unit tests is also done after a 3 seconds wait to verify if the correct response is given.
+
+import unittest
 import requests
+import time
 
-url = "https://reqres.in/api/users?page=2"
-response = requests.get(url)
+class TestGetDelayedUserList(unittest.TestCase):
 
-print(response.status_code)  # Should return a 200 code
-print(response.json())  # Returns the API response
+    def setUp(self):
+        self.url = "https://reqres.in/api/users?page=2"
+
+    def test_get_user_list(self):
+        # 3 seconds of wait is simulated before the tests are done. Otherwise the tests will fail because they are to quick.
+        time.sleep(3)
+
+        response = requests.get(self.url)
+
+        # Check if the statuscode is 200, otherwise trow an error message.
+        self.assertEqual(response.status_code, 200, "Failed to fetch user list!")
+
+        # Check if the data in the response body is a list, otherwise trow and error message
+        self.assertIsInstance(response.json()["data"], list, "Response 'data' is not a list!")
+
+        # Check if the lenght of the names in list of the response is greater than 0, otherwise trow an error message.
+        self.assertGreater(len(response.json()["data"]), 0, "User list is empty!")
+
+if __name__ == "__main__":
+    unittest.main()
--- Assignments_Leander_van_Vliet/APITests/LoginUser/LoginUser.py
+++ Assignments_Leander_van_Vliet/APITests/LoginUser/LoginUser.py
@@ -1,15 +1,27 @@
 # This request will login with a user called eve.holt. You will recieve a token to login with in the console as a response, also it will print the status code after execution. 
+# An unit test is used to confirm that a 200 message and a token is recieved in the response.
 
 
+import unittest
 import requests
 
-url = "https://reqres.in/api/login"
-data = {
-    "email": "eve.holt@reqres.in",
-    "password": "cityslicka"
-}
+class TestUserLogin(unittest.TestCase):
 
-response = requests.post(url, json=data)
+    def setUp(self):
+        self.url = "https://reqres.in/api/login"
+        self.valid_data = {
+            "email": "eve.holt@reqres.in",
+            "password": "cityslicka"
+        }
 
-print("Status Code:", response.status_code)
-print("Response:", response.json())
+    def test_successful_login(self):
+        response = requests.post(self.url, json=self.valid_data)
+
+        # Check if the response code is 200, otherwise print an error message
+        self.assertEqual(response.status_code, 200, "Login failed with valid credentials!")
+
+        # Check if the response has a "token", otherwise print an error message
+        self.assertIn("token", response.json(), "No token received after login!")
+
+if __name__ == "__main__":
+    unittest.main()
