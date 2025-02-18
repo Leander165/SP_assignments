edaba48cb51ee28f18a9f773b052288efa495d96
Leander165 <91758895+Leander165@users.noreply.github.com>
2025-02-18 16:16:41 +0100

    Add files via upload

--- Assignments_Leander_van_Vliet/Jenkins/jenkinsfile.groovy
+++ Assignments_Leander_van_Vliet/Jenkins/jenkinsfile.groovy
@@ -2,77 +2,67 @@ pipeline {
     agent any
 
     triggers {
-        cron('0 7 * * *')
+        cron('0 7 * * *') 
     }
-    
+
+    environment {
+        ROBOT_OUTPUT_DIR = 'results'
+    }
+
     stages {
         stage('Checkout Repository') {
             steps {
                 git branch: 'main', url: 'https://github.com/Leander165/SP_assignments.git'
             }
         }
+
         stage('Install Dependencies') {
             steps {
                 catchError(buildResult: 'SUCCESS') {
-                script {
-                    sh '''
-                    python3 -m venv venv
-                    . venv/bin/activate
-                    pip3 install --upgrade pip
-                    pip3 install --upgrade robotframework robotframework-seleniumlibrary
-                    python --version
-                    pip3 --version
-                    pip3 install requests
-                    '''
+                    script {
+                        sh '''
+                        python3 -m venv venv
+                        . venv/bin/activate
+                        pip install --upgrade pip
+                        pip install --upgrade robotframework robotframework-seleniumlibrary requests
+                        python --version
+                        pip --version
+                        '''
                     }
                 }
             }
         }
+
         stage('Run UI Tests') {
             steps {
                 script {
                     sh '''
                     . venv/bin/activate
-                    python -m robot -d  Assignments_Leander_van_Vliet/UITests/LoginUnitTest/LoginUnitTest.robot 
-                    python -m robot -d  Assignments_Leander_van_Vliet/UITests/LogOutUnitTest/LogOutUnitTest.robot 
-                    python -m robot -d  Assignments_Leander_van_Vliet/UITests/BuyAShirtEndToEndTest/BuyAShirtEndToEndTest.robot 
+                    robot -d results Assignments_Leander_van_Vliet/UITests/LoginUnitTest/LoginUnitTest.robot
+                    robot -d results Assignments_Leander_van_Vliet/UITests/LogOutUnitTest/LogOutUnitTest.robot
+                    robot -d results Assignments_Leander_van_Vliet/UITests/BuyAShirtEndToEndTest/BuyAShirtEndToEndTest.robot
                     '''
                 }
             }
         }
+
         stage('Run API Tests') {
             steps {
                 script {
                     sh '''
                     . venv/bin/activate
-                    python -m unittest Assignments_Leander_van_Vliet/APITests/CreateUser/CreateUser.py
-                    python -m unittest Assignments_Leander_van_Vliet/APITests/DeleteUser/DeleteUser.py
-                    python -m unittest Assignments_Leander_van_Vliet/APITests/GetDelayedUserList/GetDelayedUserlist.py
-                    python -m unittest Assignments_Leander_van_Vliet/APITests/LoginUser/LoginUser.py
+                    python -m unittest discover -s Assignments_Leander_van_Vliet/APITests -p "*.py"
                     '''
                 }
             }
         }
     }
+
     post {
-        always {
-            script {
-                failure {
-                        emailext (
-                            to: 'Thisemailisfakebutineedtoputsomething@somethingofcourse.com',
-                            subject: 'One of your jenkins tests failed',
-                            body: """You need to check your pipeline results in Jenkins to see what went wrong. Kind regards, Jenkins robot""",
-                            attachmentsPattern: 'Assignments_Leander_van_Vliet/log.html'
-                        )
-                    echo "Build result = ${currentBuild.result}"
-                   
-                }
-                success {
-                    currentBuild.result = 'SUCCESS'
-                    echo "Build result = ${currentBuild.result}, no email will be send"
-                   
-                }
-            }
-        }   
+        failure {
+            mail to: 'jouw-email@example.com',
+                 subject: "Jenkins Build Failed",
+                 body: "Build #${currentBuild.number} failed. Check logs at: ${env.BUILD_URL}"
+        }
     }
 }
